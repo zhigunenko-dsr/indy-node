69e8bc145b43f60ec2e8d108f8784b3fe891b914
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-06-23 11:32:59 +0300

    MIgrations test
    Conflicts:
    	Jenkinsfile

--- data/migrations/1_5_4_backup_proof.py
+++
@@ -1,4 +0,0 @@
-import shutil
-shutil.copyfile('/home/sovrin/sovrin_backup_1.5.1.zip', '/home/sovrin/backup_proof.zip')
-with open('/home/sovrin/.sovrin/migration_proof', 'w') as f:
-  f.write('1.5.5')
