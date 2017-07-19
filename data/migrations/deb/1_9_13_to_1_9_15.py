64116b923eddff8415dfb4e17331ce5c6a78fe40
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-07-19 16:44:38 +0300

    MIgrations test

--- data/migrations/deb/1_9_13_to_1_9_15.py
+++ data/migrations/deb/1_9_13_to_1_9_15.py
@@ -3,4 +3,5 @@ with open('/home/sovrin/.sovrin/migration_proof', 'w') as f:
   f.write('1.5.6')
 
 shutil.copytree('/home/sovrin/.sovrin', '/home/sovrin/.sovrin_test')
+shutil.rmtree('/home/sovrin/.sovrin')
 raise Exception('1.5.6test')
